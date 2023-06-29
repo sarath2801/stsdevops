847e64420483c4db2389a341f0ae034d809b95b3
sarath2801 <92496132+sarath2801@users.noreply.github.com>
2023-06-29 18:16:24 +0530

    Update setup-Jenkins.sh

--- Jenkins/jenkins/Install/setup-Jenkins.sh
+++ Jenkins/jenkins/Install/setup-Jenkins.sh
@@ -10,7 +10,7 @@ sudo apt install -y unzip wget
 
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y openjdk-11-jre
 
 #Install Jenkins
 
